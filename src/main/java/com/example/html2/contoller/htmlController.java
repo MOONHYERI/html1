cb7f0ac0e15c7b431bec0e7e629d89a636b6c696
MOONHEYRI <a6967829@naver.com>
2023-12-22 16:57:13 +0900

    추가

--- src/main/java/com/example/html2/contoller/htmlController.java
+++ src/main/java/com/example/html2/contoller/htmlController.java
@@ -1,25 +1,88 @@
 package com.example.html2.contoller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class htmlController {
 
     @GetMapping("/header")
-    public String htmlController1(){
+    public String htmlController1(Model model) {
         return "header";
     }
+
     @GetMapping("/footer")
-    public String htmlController2(){
+    public String htmlController2(Model model) {
         return "footer";
     }
+
     @GetMapping("/home")
-    public String htmlController3(){
+    public String htmlController3(Model model) {
         return "home";
     }
+
     @GetMapping("/me")
-    public String htmlController4(){ return "me";}
+    public String htmlController4(Model model) {
+        return "me";
+    }
+
     @GetMapping("/team")
-    public String htmlController5(){ return "team";}
+    public String htmlController5(Model model) {
+        return "team";
+    }
+
+    @GetMapping("/location")
+    public String htmlController6(Model model) {
+        return "location";
+    }
+
+    @GetMapping("/boardinfo")
+    public String htmlController7(Model model) {
+        return "boardinfo";
+    }
+
+    @GetMapping("/board1")
+    public String htmlController8(Model model) {
+        return "board1";
+    }
+
+    @GetMapping("/board")
+    public String htmlController9(Model model) {
+        return "board";
+    }
+
+    @GetMapping("/login")
+    public String htmlController10(Model model) {
+        return "login";
+    }
+
+    @GetMapping("/join")
+    public String htmlController11(Model model) {
+        return "join";
+    }
+
+    @GetMapping("/editmy")
+    public String htmlController12(Model model) {
+        return "editmy";
+    }
+
+    @GetMapping("/boardwrite")
+    public String htmlController14(Model model) {
+        return "boardwrite";
+    }
+
+    @GetMapping("/boardwrite1")
+    public String htmlController15(Model model) {
+        return "boardwrite1";
+    }
+
+    @GetMapping("/map")
+    public String htmlContorller16(Model model) {
+        return "map";
+    }
+    @GetMapping("/updateorigin")
+    public String htmlController17(Model model){
+        return "updateorigin";
+    }
 }
